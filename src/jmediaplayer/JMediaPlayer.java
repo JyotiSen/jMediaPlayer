14c2a94ddb826388da9e1bf06529597a4b262588
OMPRAKASH SEN <OMPRAKASH SEN@dell>
2018-05-23 13:45:35 +0530

    Initial commit

---
+++ src/jmediaplayer/JMediaPlayer.java
@@ -0,0 +1,40 @@
+package jmediaplayer;
+
+import javafx.scene.input.MouseEvent;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author OMPRAKASH SEN
+ */
+public class JMediaPlayer extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        stage.setTitle("jMediaPlayer");
+        scene.setOnMouseClicked ((MouseEvent doubleClicked) -> {
+            if(doubleClicked.getClickCount()==2)
+            {
+                stage.setFullScreen(true);
+            }
+        });
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
